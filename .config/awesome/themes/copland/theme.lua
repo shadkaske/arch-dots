d540440f550a3bc4e5c87f760d2937d0c97989fa
Shad Kaske <shadkaske@gmail.com>
2021-03-14 10:40:30 -0500

    Zsh, Awesome Configs

---
+++ .config/awesome/themes/copland/theme.lua
@@ -0,0 +1,375 @@
+--[[
+
+     Copland Awesome WM theme 2.0
+     github.com/lcpz
+
+--]]
+
+local gears = require("gears")
+local lain  = require("lain")
+local awful = require("awful")
+local wibox = require("wibox")
+local dpi   = require("beautiful.xresources").apply_dpi
+
+local awesome, client, os = awesome, client, os
+local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
+
+local color00                                   = "#263238"
+local color01                                   = "#EC5F67"
+local color02                                   = "#8BD649"
+local color03                                   = "#FFCC00"
+local color04                                   = "#89DDFF"
+local color05                                   = "#82AAFF"
+local color06                                   = "#80CBC4"
+local color07                                   = "#CDD3DE"
+local color08                                   = "#707880"
+local color09                                   = color01
+local color10                                   = color02
+local color11                                   = color03
+local color12                                   = color04
+local color13                                   = color05
+local color14                                   = color06
+local color15                                   = "#FFFFFF"
+local color16                                   = "#EA9560"
+local color17                                   = "#EC5F67"
+local color18                                   = "#2C393F"
+local color19                                   = "#37474F"
+local color20                                   = "#C9CCD3"
+local color21                                   = "#D5DBE5"
+local color_foreground                          = color07
+local color_background                          = color00
+
+local background                                = color_background
+local foreground                                = color_foreground
+local foreground_inactive                       = color08
+local highlight                                 = color05
+local urgent                                    = color01
+local background_alt                            = color08
+
+local theme                                     = {}
+theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/copland"
+theme.font                                      = "Cantarell 10"
+theme.fg_normal                                 = foreground
+theme.fg_focus                                  = foreground
+theme.bg_normal                                 = background
+theme.bg_focus                                  = background
+theme.fg_urgent                                 = foreground
+theme.bg_urgent                                 = urgent
+theme.border_width                              = dpi(2)
+theme.border_normal                             = background_alt
+theme.border_focus                              = highlight
+theme.taglist_fg_focus                          = background
+theme.taglist_bg_focus                          = highlight
+theme.taglist_bg_empty                          = background
+theme.taglist_fg_empty                          = foreground_inactive
+theme.taglist_bg_occupied                       = background
+theme.taglist_fg_occupied                       = foreground
+theme.titlebar_bg_normal                        = theme.bg_normal
+theme.titlebar_bg_focus                         = theme.bg_focus
+theme.menu_height                               = dpi(16)
+theme.menu_width                                = dpi(130)
+theme.tasklist_disable_icon                     = true
+theme.awesome_icon                              = theme.dir .."/icons/awesome.png"
+theme.menu_submenu_icon                         = theme.dir .. "/icons/submenu.png"
+theme.taglist_squares_sel                       = theme.dir .. "/icons/square_unsel.png"
+theme.taglist_squares_unsel                     = theme.dir .. "/icons/square_unsel.png"
+theme.vol                                       = theme.dir .. "/icons/vol.png"
+theme.vol_low                                   = theme.dir .. "/icons/vol_low.png"
+theme.vol_no                                    = theme.dir .. "/icons/vol_no.png"
+theme.vol_mute                                  = theme.dir .. "/icons/vol_mute.png"
+theme.disk                                      = theme.dir .. "/icons/disk.png"
+theme.ac                                        = theme.dir .. "/icons/ac.png"
+theme.bat                                       = theme.dir .. "/icons/bat.png"
+theme.bat_low                                   = theme.dir .. "/icons/bat_low.png"
+theme.bat_no                                    = theme.dir .. "/icons/bat_no.png"
+theme.play                                      = theme.dir .. "/icons/play.png"
+theme.pause                                     = theme.dir .. "/icons/pause.png"
+theme.stop                                      = theme.dir .. "/icons/stop.png"
+theme.layout_tile                               = theme.dir .. "/icons/tile.png"
+theme.layout_tileleft                           = theme.dir .. "/icons/tileleft.png"
+theme.layout_tilebottom                         = theme.dir .. "/icons/tilebottom.png"
+theme.layout_tiletop                            = theme.dir .. "/icons/tiletop.png"
+theme.layout_fairv                              = theme.dir .. "/icons/fairv.png"
+theme.layout_fairh                              = theme.dir .. "/icons/fairh.png"
+theme.layout_spiral                             = theme.dir .. "/icons/spiral.png"
+theme.layout_dwindle                            = theme.dir .. "/icons/dwindle.png"
+theme.layout_max                                = theme.dir .. "/icons/max.png"
+theme.layout_fullscreen                         = theme.dir .. "/icons/fullscreen.png"
+theme.layout_magnifier                          = theme.dir .. "/icons/magnifier.png"
+theme.layout_floating                           = theme.dir .. "/icons/floating.png"
+theme.useless_gap                               = 4
+theme.master_width_factor                       = .55
+theme.titlebar_close_button_focus               = theme.dir .. "/icons/titlebar/close_focus.png"
+theme.titlebar_close_button_normal              = theme.dir .. "/icons/titlebar/close_normal.png"
+theme.titlebar_ontop_button_focus_active        = theme.dir .. "/icons/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active       = theme.dir .. "/icons/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive      = theme.dir .. "/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive     = theme.dir .. "/icons/titlebar/ontop_normal_inactive.png"
+theme.titlebar_sticky_button_focus_active       = theme.dir .. "/icons/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active      = theme.dir .. "/icons/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive     = theme.dir .. "/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive    = theme.dir .. "/icons/titlebar/sticky_normal_inactive.png"
+theme.titlebar_floating_button_focus_active     = theme.dir .. "/icons/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active    = theme.dir .. "/icons/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive   = theme.dir .. "/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive  = theme.dir .. "/icons/titlebar/floating_normal_inactive.png"
+theme.titlebar_maximized_button_focus_active    = theme.dir .. "/icons/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active   = theme.dir .. "/icons/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.dir .. "/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = theme.dir .. "/icons/titlebar/maximized_normal_inactive.png"
+
+-- My Icon Addtions
+theme.eth_icon                                       = theme.dir .. "/icons/net_up.png"
+
+-- lain related
+theme.layout_centerfair                         = theme.dir .. "/icons/centerfair.png"
+theme.layout_termfair                           = theme.dir .. "/icons/termfair.png"
+theme.layout_centerwork                         = theme.dir .. "/icons/centerwork.png"
+
+local markup = lain.util.markup
+local blue   = highlight
+local red    = urgent
+local green  = color02
+
+-- Textclock
+--os.setlocale(os.getenv("LANG")) -- to localize the clock
+local mytextclock = wibox.widget.textclock(markup(foreground, " %a %b %d %R "))
+mytextclock.font = theme.font
+
+-- Calendar
+theme.cal = lain.widget.cal({
+    attach_to = { mytextclock },
+    notification_preset = {
+        font = "JetBrainsMono Nerd Font 11",
+        fg   = foreground,
+        bg   = background
+    }
+})
+
+-- MPD
+local mpdicon = wibox.widget.imagebox()
+theme.mpd = lain.widget.mpd({
+    settings = function()
+        if mpd_now.state == "play" then
+            title = mpd_now.title .. " "
+            artist  = " " .. markup(foreground, mpd_now.artist)  .. markup(foreground_inactive, " <span font='Terminus 2'> </span><span font='Terminus 5'> </span>")
+            mpdicon:set_image(theme.play)
+        elseif mpd_now.state == "pause" then
+            title = "mpd "
+            artist  = "paused " .. markup(foreground_inactive, " <span font='Terminus 5'> </span>")
+            mpdicon:set_image(theme.pause)
+        else
+            title  = ""
+            artist = ""
+            mpdicon._private.image = nil
+            mpdicon:emit_signal("widget::redraw_needed")
+            mpdicon:emit_signal("widget::layout_changed")
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(blue, title) .. artist))
+    end
+})
+
+-- Battery
+local baticon = wibox.widget.imagebox(theme.bat)
+local batbar = wibox.widget {
+    forced_height    = dpi(1),
+    forced_width     = dpi(59),
+    color            = theme.fg_normal,
+    background_color = theme.bg_normal,
+    margins          = 1,
+    paddings         = 1,
+    ticks            = true,
+    ticks_size       = dpi(6),
+    widget           = wibox.widget.progressbar,
+}
+-- local batupd = lain.widget.bat({
+--     settings = function()
+--         if (not bat_now.status) or bat_now.status == "N/A" or type(bat_now.perc) ~= "number" then return end
+
+--         if bat_now.status == "Charging" then
+--             baticon:set_image(theme.ac)
+--             if bat_now.perc >= 98 then
+
+-- Pulse volume bar
+local volicon = wibox.widget.imagebox(theme.vol)
+theme.volume = lain.widget.pulsebar {
+    width = dpi(59), border_width = 0, ticks = true, ticks_size = dpi(4),
+    notification_preset = { font = theme.font },
+    --togglechannel = "IEC958,3",
+    settings = function()
+        if volume_now.muted == "yes" then
+            volicon:set_image(theme.vol_mute)
+        else
+            volicon:set_image(theme.vol)
+        end
+    end,
+    colors = {
+        background   = background_alt,
+        mute         = urgent,
+        unmute       = foreground
+    }
+}
+theme.volume.tooltip.wibox.fg = theme.fg_focus
+theme.volume.bar:buttons(my_table.join (
+          awful.button({}, 1, function()
+            os.execute("pactl set-sink-mute @DEFAULT_SINK@ toggle")
+            theme.volume.update()
+          end),
+          awful.button({}, 3, function()
+            awful.spawn(string.format("pavucontrol --tab 3", awful.util.terminal))
+          end),
+          awful.button({}, 4, function()
+            os.execute("pactl set-sink-volume @DEFAULT_SINK@ +5%")
+            theme.volume.update()
+          end),
+          awful.button({}, 5, function()
+            os.execute("pactl set-sink-volume @DEFAULT_SINK@ -5%")
+            theme.volume.update()
+          end)
+))
+local volumebg = wibox.container.background(theme.volume.bar, background, gears.shape.rectangle)
+local volumewidget = wibox.container.margin(volumebg, dpi(2), dpi(7), dpi(4), dpi(4))
+
+-- Weather
+local myweather = lain.widget.weather({
+    city_id = 5231851,
+    units = "imperial"
+})
+
+-- Separators
+local first     = wibox.widget.textbox(markup.font("Terminus 3", " "))
+local spr       = wibox.widget.textbox(' ')
+local small_spr = wibox.widget.textbox(markup.font("Terminus 4", " "))
+local bar_spr   = wibox.widget.textbox(markup.font("Terminus 3", " ") .. markup.fontfg(theme.font, foreground_inactive, "|") .. markup.font("Terminus 5", " "))
+
+-- {{{ Filter Taglist to show only used tags
+-- local orig_filter = awful.widget.taglist.filter.all
+
+-- -- Taglist label functions
+-- awful.widget.taglist.filter.all = function (t, args)
+--     if t.selected or #t:clients() > 0 then
+--         return orig_filter(t, args)
+--     end
+-- end
+-- }}}
+
+function theme.at_screen_connect(s)
+    -- Quake application
+    s.quake = lain.util.quake({ app = "kitty",argname = "--title %s",extra = "--class=QuakeDD tmux new-session -A -s DropDown",
+            visible = true, height = 0.4, width = 0.4, vert = "top", horiz = "center"})
+
+    -- Tags
+    awful.tag(awful.util.tagnames, s, awful.layout.layouts[1])
+
+    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(my_table.join(
+                           awful.button({}, 1, function () awful.layout.inc( 1) end),
+                           awful.button({}, 2, function () awful.layout.set( awful.layout.layouts[1] ) end),
+                           awful.button({}, 3, function () awful.layout.inc(-1) end),
+                           awful.button({}, 4, function () awful.layout.inc( 1) end),
+                           awful.button({}, 5, function () awful.layout.inc(-1) end)))
+
+    -- Create a taglist widget
+    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons)
+
+    -- Create a tasklist widget
+    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, awful.util.tasklist_buttons)
+
+    -- Network Icons
+    -- local wifi_icon = wibox.widget.imagebox()
+    -- local pia_icon = wibox.widget.imagebox()
+    -- local vpn_icon = wibox.widget.imagebox()
+    local eth_icon = wibox.widget.imagebox()
+    local net = lain.widget.net {
+        notify = "off",
+        wifi_state = "on",
+        eth_state = "on",
+        settings = function()
+            local enp6s0 = net_now.devices.enp6s0
+            if enp6s0 then
+                if enp6s0.ethernet then
+                    eth_icon:set_image(eth_icon)
+                else
+                    eth_icon:set_image()
+                end
+            end
+
+            -- local wlan0 = net_now.devices.wlan0
+            -- if wlan0 then
+            --     if wlan0.wifi then
+            --         local signal = wlan0.signal
+            --         if signal < -83 then
+            --             wifi_icon:set_image(wifi_weak_filename)
+            --         elseif signal < -70 then
+            --             wifi_icon:set_image(wifi_mid_filename)
+            --         elseif signal < -53 then
+            --             wifi_icon:set_image(wifi_good_filename)
+            --         elseif signal >= -53 then
+            --             wifi_icon:set_image(wifi_great_filename)
+            --         end
+            --     else
+            --         wifi_icon:set_image()
+            --     end
+            -- end
+        end
+    }
+
+    -- Systray
+    s.systray = wibox.widget.systray()
+    s.systray.visible = true
+    s.systray.force_height = 12
+
+    -- Create the wibox
+    s.mywibox = awful.wibar({
+        position = "top",
+        screen = s,
+        height = dpi(24),
+        bg = theme.bg_normal,
+        fg = theme.fg_normal,
+        opacity = 0.92
+    })
+
+    -- Add widgets to the wibox
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            small_spr,
+            s.mylayoutbox,
+            first,
+            small_spr,
+            s.mytaglist,
+            -- first,
+            -- s.mypromptbox,
+        },
+        s.mytasklist, -- Middle widget
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            --theme.mail.widget,
+            mpdicon,
+            theme.mpd.widget,
+            spr,
+            -- baticon,
+            -- batwidget,
+            -- bar_spr,
+            --fsicon,
+            --fswidget,
+            -- bar_spr,
+            small_spr,
+            volicon,
+            volumewidget,
+            small_rpr,
+            spr,
+            spr,
+            mytextclock,
+            small_spr,
+            s.systray,
+            small_spr
+        },
+    }
+end
+
+return theme
---
+++ .zshrc
@@ -0,0 +1,163 @@
+# Source Profile
+[ -f $HOME/.profile ] && source $HOME/.profile
+
+# Source zsh_env
+[ -f $HOME/.zsh_env ] && source $HOME/.zsh_env
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -n $SSH_CONNECTION ]]; then
+    ZSH_THEME="robbyrussell"
+else
+    if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+    fi
+    ZSH_THEME="powerlevel10k/powerlevel10k"
+fi
+
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/.oh-my-zsh
+
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/"
+[ -n "$PS1" ] && \
+    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+        eval "$("$BASE16_SHELL/profile_helper.sh")"
+
+# Set default color scheme if base16-shell link is missing
+[ ! -f $HOME/.base16_theme ] && base16_helios
+
+HYPHEN_INSENSITIVE="true"
+
+COMPLETION_WAITING_DOTS="true"
+
+plugins=(
+    zsh-syntax-highlighting
+    zsh-autosuggestions
+    git
+    git-flow
+    archlinux
+    tmux
+    tmuxinator
+    sudo
+    systemd
+    composer
+    laravel
+    laravel5
+    vagrant
+    common-aliases
+    vi-mode
+)
+
+# User configuration
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+source $ZSH/oh-my-zsh.sh
+
+# Preferred editor for local and remote sessions
+export EDITOR='nvim'
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='vi'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# ssh
+# export SSH_KEY_PATH="~/.ssh/dsa_id"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+alias zr="source ~/.zshrc"
+alias minicom="minicom -c on"
+alias tinker="php artisan tinker"
+alias phu="vendor/bin/phpunit"
+alias phuf="vendor/bin/phpunit --filter "
+alias tt='tmux new-session -A -s Terms'
+alias tp='tmuxinator start printApp-Homestead'
+alias nivm='nvim'
+alias eixt='exit'
+alias cl="cal -B 1 -A 1"
+alias xoff='sudo phpdismod -s cli xdebug'
+alias xon='sudo phpenmod -s cli xdebug'
+alias vupp='vagrant up --provision'
+alias lzg=lazygit
+alias tn='tmux new-session -A -s'
+alias lzgd='lazygit -w $HOME -g $HOME/.dots'
+
+export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.local/bin/pip:$PATH
+export PATH=$HOME/.config/composer/vendor/bin:$PATH
+export PATH=$HOME/.composer/vendor/bin:$PATH
+
+# Term Setting
+export TERM="xterm-256color"
+
+# AskPass Helper
+export SUDO_ASKPASS='/usr/bin/ssh-askpass'
+
+# Vi Mode Cursor change
+function zle-keymap-select zle-line-init
+{
+    # change cursor shape in iTerm2
+    case $KEYMAP in
+        vicmd)      print -n --  '\033[1 q';;  # block cursor
+        viins|main) print -n -- '\033[5 q';;  # line cursor
+    esac
+
+    zle reset-prompt
+    zle -R
+}
+
+function zle-line-finish
+{
+    print -n -- '\033[1 q'  # block cursor
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
+
+# Custom Keybinds
+
+# Remap up and down are history search
+bindkey '^[[A' up-line-or-search
+bindkey '^[[B' down-line-or-search
+
+# Ctrl Space to accept autosuggestions
+bindkey '^ ' autosuggest-accept
+
+# Custom functions
+
+function homestead() {
+    ( cd ~/Homestead && vagrant $* )
+}
+
+function kkssh() {
+    ( kitty +kitten ssh $1 && ssh $1 )
+}
+
+function dots {
+   /usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME $@
+}
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# Set FZF Options
+export FZF_COMPLETION_OPTS='--border --info=inline'
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# Fix for systemd unit file completion
+_systemctl_unit_state() {
+  typeset -gA _sys_unit_state
+  _sys_unit_state=( $(__systemctl list-unit-files "$PREFIX*" | awk '{print $1, $2}') ) }
